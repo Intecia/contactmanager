b0da6c9ec9ebb1f8528fbcf5108e9a02452091e5
Narinder Kumar <loveapur@gmail.com>
2019-09-23 19:55:08 +0530

    Updates

---
+++ precache-manifest.817721d6368a3b3931d42319c61e0046.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee31b85dff769ea4f227363202d0d40f",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "fcd8ef3c68a88402b0a4",
+    "url": "/contactmanager/static/css/2.64d4a9e9.chunk.css"
+  },
+  {
+    "revision": "1b12d423c9151e53dbc9",
+    "url": "/contactmanager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fcd8ef3c68a88402b0a4",
+    "url": "/contactmanager/static/js/2.2d334dae.chunk.js"
+  },
+  {
+    "revision": "1b12d423c9151e53dbc9",
+    "url": "/contactmanager/static/js/main.75a1fa26.chunk.js"
+  },
+  {
+    "revision": "440bac45752b04fd4578",
+    "url": "/contactmanager/static/js/runtime~main.43891527.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/contactmanager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/contactmanager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/contactmanager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/contactmanager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/contactmanager/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
